c91fe542a37e0b62ca92aaf51c949b3b4582034f
rabby-web <rabby.webdeveloper@gmail.com>
2024-02-05 22:04:34 +0600

    Content Marketing modal add

---
+++ src/components/page/Service/CM.jsx
@@ -0,0 +1,139 @@
+import { HiOutlineChevronDoubleRight } from "react-icons/hi";
+
+export default function CM() {
+  return (
+    <>
+      <div class="w-full bg-white border border-gray-200 rounded-lg  dark:bg-gray-800 dark:border-gray-700 shadow-[rgba(50,50,93,0.25)_0px_6px_12px_-2px,_rgba(0,0,0,0.3)_0px_3px_7px_-3px]">
+        <a href="#">
+          <img
+            class="rounded-t-lg h-52 w-full object-cover"
+            src="https://img.freepik.com/free-vector/people-with-digital-content-creation-concept_53876-43057.jpg?t=st=1707149006~exp=1707149606~hmac=91de374bd286d2fd9fd1166628c7fc6fbcdaf0c86ae09206e0b07b2f62590274"
+            alt=""
+          />
+        </a>
+        <div class="p-5">
+          <a href="#">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Content Marketing:
+            </h5>
+          </a>
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Content marketing is a marketing strategy used to attract, engage,
+            and retain an audience by creating and sharing relevant articles,
+            videos, podcasts, and other media.
+          </p>
+
+          <button
+            onClick={() => document.getElementById("my_modal_4").showModal()}
+          >
+            <a
+              href="#_"
+              class="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-indigo-600 transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-50 group"
+            >
+              <span class="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-indigo-600 group-hover:h-full"></span>
+              <span class="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
+                <svg
+                  class="w-5 h-5 text-green-400"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M14 5l7 7m0 0l-7 7m7-7H3"
+                  ></path>
+                </svg>
+              </span>
+              <span class="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
+                <svg
+                  class="w-5 h-5 text-green-400"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M14 5l7 7m0 0l-7 7m7-7H3"
+                  ></path>
+                </svg>
+              </span>
+              <span class="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">
+                Details
+              </span>
+            </a>
+          </button>
+        </div>
+        <dialog id="my_modal_4" className="modal ">
+          <div className="modal-box bg-slate-200 dark:bg-dark-01 dark:text-white text-black m-0 p-2 rounded-lg">
+            <h3 className="font-bold text-2xl text-violet-800 dark:text-purple-600">
+              Content Marketing:
+            </h3>
+            <div className="p-2 mb-1 mt-2 text-lg font-medium">
+              <h3>
+                Content marketing is a marketing strategy used to attract,
+                engage, and retain an audience by creating and sharing relevant
+                articles, videos, podcasts, and other media.
+              </h3>
+            </div>
+            <p className="py-4 text-lg">
+              <span className="p-1 flex gap-3">
+                <HiOutlineChevronDoubleRight className="text-4xl font-bold text-violet-800 dark:text-purple-600" />
+                <div>
+                  <span className="text-lg font-semibold">
+                    Content Strategy:
+                  </span>{" "}
+                  Develop a comprehensive content plan aligned with your
+                  business goals and target audience.
+                </div>
+              </span>
+              <span className="p-1 flex gap-3">
+                <HiOutlineChevronDoubleRight className="text-4xl font-bold text-violet-800 dark:text-purple-600" />
+                <div>
+                  <span className="text-lg font-semibold">
+                    Content Creation:
+                  </span>{" "}
+                  Produce high-quality blog posts, articles, ebooks,
+                  infographics, and other valuable content assets.
+                </div>
+              </span>
+              <span className="p-1 flex gap-3">
+                <HiOutlineChevronDoubleRight className="text-4xl font-bold text-violet-800 dark:text-purple-600" />
+                <div>
+                  <span className="text-lg font-semibold">
+                    Content Distribution:{" "}
+                  </span>{" "}
+                  Promote content through various channels, including social
+                  media, email newsletters, and content syndication.
+                </div>
+              </span>
+              <span className="p-1 flex gap-3">
+                <HiOutlineChevronDoubleRight className="text-4xl font-bold text-violet-800 dark:text-purple-600" />
+                <div>
+                  <span className="text-lg font-semibold">
+                    Performance Analysis:
+                  </span>{" "}
+                  Measure content performance using metrics such as website
+                  traffic, engagement, and lead generation to refine strategies.
+                </div>
+              </span>
+            </p>
+            <div className="modal-action">
+              <form method="dialog">
+                {/* if there is a button in form, it will close the modal */}
+                <button className="btn bg-violet-700 dark:bg-purple-600 text-white -mt-2">
+                  Close
+                </button>
+              </form>
+            </div>
+          </div>
+        </dialog>
+      </div>
+    </>
+  );
+}
