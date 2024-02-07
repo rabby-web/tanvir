cea55621c02d8d146159f1d50c281084a904a348
rabby-web <rabby.webdeveloper@gmail.com>
2024-02-07 18:29:45 +0600

    project section add

--- src/components/page/Project/Project.jsx
+++ src/components/page/Project/Project.jsx
@@ -4,53 +4,131 @@ export default function Project() {
   return (
     <>
       <SectionTitle heading="Project" subHeading="Latest Work"></SectionTitle>
-      <div class="container mx-auto px-5 py-2 lg:px-32 lg:pt-24 mb-8">
-        <div class="-m-1 flex flex-wrap md:-m-2">
-          <div class="flex w-1/2 flex-wrap">
-            <div class="w-1/2 p-1 md:p-2">
-              <img
-                alt="gallery"
-                class="block h-full w-full rounded-lg object-cover object-center"
-                src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp"
-              />
-            </div>
-            <div class="w-1/2 p-1 md:p-2">
-              <img
-                alt="gallery"
-                class="block h-full w-full rounded-lg object-cover object-center"
-                src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp"
-              />
-            </div>
-            <div class="w-full p-1 md:p-2">
-              <img
-                alt="gallery"
-                class="block h-full w-full rounded-lg object-cover object-center"
-                src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-              />
-            </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-16 mx-4">
+        {/* 1------- */}
+        <div className="bg-white border rounded-md ">
+          <div className=" room overflow-hidden relative">
+            <img
+              // onClick={handleDetails}
+              className="scale-110 hover:scale-100 duration-700 -z-10 cursor-pointer room-image w-full h-64 mb-4"
+              src="https://i.ibb.co/7nQmcYP/p-1.jpg"
+              alt="Image 3"
+            />
+            <h2
+              className=" pr-10 rounded-sm room-title text-lg font-medium text-dark-01 bg-white hover:bg-slate-100 z-10 absolute left-0 bottom-0 py-1.5 px-4 duration-700  w-full"
+              data-aos="fade-up"
+              data-aos-duration="2000"
+            >
+              Name: Views and Impression
+              <span className=" text-dark-01 mb-2 block text-sm">
+                Company: <span className="text-base">Daraz Online Shop</span>
+              </span>
+            </h2>
           </div>
-          <div class="flex w-1/2 flex-wrap">
-            <div class="w-full p-1 md:p-2">
-              <img
-                alt="gallery"
-                class="block h-full w-full rounded-lg object-cover object-center"
-                src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp"
-              />
-            </div>
-            <div class="w-1/2 p-1 md:p-2">
-              <img
-                alt="gallery"
-                class="block h-full w-full rounded-lg object-cover object-center"
-                src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp"
-              />
-            </div>
-            <div class="w-1/2 p-1 md:p-2">
-              <img
-                alt="gallery"
-                class="block h-full w-full rounded-lg object-cover object-center"
-                src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(77).webp"
-              />
-            </div>
+        </div>
+        {/* 2------- */}
+        <div className="bg-white border rounded-md ">
+          <div className=" room overflow-hidden relative">
+            <img
+              // onClick={handleDetails}
+              className="scale-110 hover:scale-100 duration-700 -z-10 cursor-pointer room-image w-full h-64 mb-4"
+              src="https://i.ibb.co/xf32rmr/p-2.jpg"
+              alt="Image 3"
+            />
+            <h2
+              className=" pr-10 rounded-sm room-title text-lg font-medium text-dark-01 bg-white hover:bg-slate-100 z-10 absolute left-0 bottom-0 py-1.5 px-4 duration-700  w-full"
+              data-aos="fade-up"
+              data-aos-duration="2000"
+            >
+              Name: Views and Impression
+              <span className=" text-dark-01 mb-2 block text-sm">
+                Company: <span className="text-base">Daraz Online Shop</span>
+              </span>
+            </h2>
+          </div>
+        </div>
+        {/* 3------- */}
+        <div className="bg-white border rounded-md ">
+          <div className=" room overflow-hidden relative">
+            <img
+              // onClick={handleDetails}
+              className="scale-110 hover:scale-100 duration-700 -z-10 cursor-pointer room-image w-full h-64 mb-4"
+              src="https://i.ibb.co/PDcyhV2/p-3.jpg"
+              alt="Image 3"
+            />
+            <h2
+              className=" pr-10 rounded-sm room-title text-lg font-medium text-dark-01 bg-white hover:bg-slate-100 z-10 absolute left-0 bottom-0 py-1.5 px-4 duration-700  w-full"
+              data-aos="fade-up"
+              data-aos-duration="2000"
+            >
+              Name: Views and Impression
+              <span className=" text-dark-01 mb-2 block text-sm">
+                Company: <span className="text-base">Daraz Online Shop</span>
+              </span>
+            </h2>
+          </div>
+        </div>
+        {/* 4------- */}
+        <div className="bg-white border rounded-md ">
+          <div className=" room overflow-hidden relative">
+            <img
+              // onClick={handleDetails}
+              className="scale-110 hover:scale-100 duration-700 -z-10 cursor-pointer room-image w-full h-64 mb-4"
+              src="https://i.ibb.co/kyZJL0m/p-4.jpg"
+              alt="Image 3"
+            />
+            <h2
+              className=" pr-10 rounded-sm room-title text-lg font-medium text-dark-01 bg-white hover:bg-slate-100 z-10 absolute left-0 bottom-0 py-1.5 px-4 duration-700  w-full"
+              data-aos="fade-up"
+              data-aos-duration="2000"
+            >
+              Name: Views and Impression
+              <span className=" text-dark-01 mb-2 block text-sm">
+                Company: <span className="text-base">Daraz Online Shop</span>
+              </span>
+            </h2>
+          </div>
+        </div>
+        {/* 5------- */}
+        <div className="bg-white border rounded-md ">
+          <div className=" room overflow-hidden relative">
+            <img
+              // onClick={handleDetails}
+              className="scale-110 hover:scale-100 duration-700 -z-10 cursor-pointer room-image w-full h-64 mb-4"
+              src="https://i.ibb.co/fC3SfNC/p-5.jpg"
+              alt="Image 3"
+            />
+            <h2
+              className=" pr-10 rounded-sm room-title text-lg font-medium text-dark-01 bg-white hover:bg-slate-100 z-10 absolute left-0 bottom-0 py-1.5 px-4 duration-700  w-full"
+              data-aos="fade-up"
+              data-aos-duration="200"
+            >
+              Name: Views and Impression
+              <span className=" text-dark-01 mb-2 block text-sm">
+                Company: <span className="text-base">Daraz Online Shop</span>
+              </span>
+            </h2>
+          </div>
+        </div>
+        {/* 6------- */}
+        <div className="bg-white border rounded-md ">
+          <div className=" room overflow-hidden relative">
+            <img
+              // onClick={handleDetails}
+              className="scale-110 hover:scale-100 duration-700 -z-10 cursor-pointer room-image w-full h-64 mb-4"
+              src="https://i.ibb.co/2tgj9Wq/p-6.jpg"
+              alt="Image 3"
+            />
+            <h2
+              className=" pr-10 rounded-sm room-title text-lg font-medium text-dark-01 bg-white hover:bg-slate-100 z-10 absolute left-0 bottom-0 py-1.5 px-4 duration-700  w-full"
+              data-aos="fade-up"
+              data-aos-duration="2000"
+            >
+              Name: Views and Impression
+              <span className=" text-dark-01 mb-2 block text-sm">
+                Company: <span className="text-base">Daraz Online Shop</span>
+              </span>
+            </h2>
           </div>
         </div>
       </div>
